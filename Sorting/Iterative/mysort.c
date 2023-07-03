4941237d255cbe243eef5894d909a034de535254
trilokeshdas <b422065@iiit-bh.ac.in>
2023-07-04 00:37:58 +0530

    init commit

--- Sorting/Iterative/mysort.c
+++ Sorting/Iterative/mysort.c
@@ -2,26 +2,35 @@
 #include<stdlib.h>
 
 void sort(int *a);
-
+void display(int *a);
 int main(){
     int a[100]={5,1,4,2,8};
-    int i;
     sort(a);
-    for(i=0;i<5;i++){
-        printf("%d\n",a[i]);
-    }
     return 0;
+}
+void display(int *a){
+  for(int i=0; i<5; i++){
+    printf("%d ",a[i]);
+  }
+  printf("\n");
 }
 void sort(int *a){
 int i,j;
+int sorted=0;
     for(i=0;i<5;i++){
+      sorted=1;
         for(j=i; j<5; j++){
           if(a[i]>a[j]){
               int temp;
             temp=a[i];
             a[i]=a[j];
             a[j]=temp;
+            sorted=0;
           }
         }
+        if(sorted){
+          return;
+        }
+        display(a);
 }
 }
