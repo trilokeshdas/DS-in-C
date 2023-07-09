81400101ef1e90363d7a945f8d60139c40ca720a
trilokeshdas <b422065@iiit-bh.ac.in>
2023-07-09 11:41:46 +0530

    init new

---
+++ Qshots/flattenBT.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *left,*right;
+    int level;
+};
+typedef struct node node;
+void createtree(int x);
+int main(){
+    
+    return 0;
+}
+void createtree(int key){
+    
+}
