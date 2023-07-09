c803efdaf9652f76e159210e85988cc7a0d72300
trilokeshdas <b422065@iiit-bh.ac.in>
2023-07-09 11:43:23 +0530

    init change

---
+++ Qshots/spiralmatrix.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+const int m=3;
+const int n=3;
+void spiral();
+int main(){
+    int a[m][n]={{1,2,3},
+                 {4,5,6},
+                 {7,8,9}};
+    return 0;
+}
+void spiral(){
+    
+}
